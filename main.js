f8de524d6549b8e397d2f66614676699cad61f80
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-02-06 00:20:38 +0100

    update

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+AOS.init();
 
+const navbar = document.querySelector('.navbar');
+let lastScrollY = window.scrollY;
 
+window.addEventListener('scroll', () => {
+  if (lastScrollY < window.scrollY) {
+    navbar.classList.add('nav-hidden');
+  } else {
+    navbar.classList.remove('nav-hidden');
 
+    if (window.scrollY === 0) {
+      navbar.style.backgroundColor = ''; 
+    } else {
+      navbar.style.backgroundColor = '#f2f2f2'; 
+    }
+  }
+  lastScrollY = window.scrollY;
+});
